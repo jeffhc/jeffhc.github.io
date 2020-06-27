ef78ebaa04bbda9c7a2389d99db87ee2addf881a
jeffhc <jhccao@gmail.com>
2020-06-27 18:41:46 -0400

    react build

---
+++ precache-manifest.19d7c0776a783547c02528635d965389.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e83e0a77fee851f3ecdac8fec17f4a6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ebc010a6e9b58519304e",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "2f0a086e8bd26162c874",
+    "url": "/static/js/2.b3c081d0.chunk.js"
+  },
+  {
+    "revision": "35e9035c4541049d41232fe0bf96f70b",
+    "url": "/static/js/2.b3c081d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ebc010a6e9b58519304e",
+    "url": "/static/js/main.413f785e.chunk.js"
+  },
+  {
+    "revision": "faa4660fa4d66ee61b5d",
+    "url": "/static/js/runtime-main.8e4ca937.js"
+  }
+]);
